b93f22ff6d544df5a5625f2888a892b7dc6d4166
tusharb-lokal <tushar.b@getlokalapp.com>
2026-09-13 10:37:18 +0530

    Update package dependencies and enhance Vite configuration for better build optimization
    - Added `rollup-plugin-visualizer` to dependencies for visualizing bundle size.
    - Updated Vite configuration to include the visualizer plugin and optimized build output by creating a separate vendor chunk for React-related libraries.
    - Refactored `App.jsx` to use React's `lazy` for dynamic imports of `ProductListing` and `UserDashboard` components, improving initial load performance.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,32 @@
 import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from "rollup-plugin-visualizer"
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), visualizer({
+    open: true
+  })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (
+            id.includes('node_modules/react/') ||
+            id.includes('node_modules/react-dom/') ||
+            id.includes('node_modules/@tanstack/react-query/')
+          ) {
+            return 'vendor'
+          }
+        },
+      },
     },
   },
 })
